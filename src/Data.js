c4c321df51a37b08acf08c8dd996ab70d7939dbb
tsedi lemma <tsedenawit3@gmail.com>
2024-07-08 10:19:53 +0300

    sent

--- src/Data.js
+++ src/Data.js
@@ -13,13 +13,33 @@ export const Data =[
     ans:"South africa"},
     {
     IDE:4 ,
-    que:"In which country are the world’s 10 coldest cities located? - russia",
-    ans:"Russia"},
+    que:"Thanks to its overseas territories, which country technically spans 12 time zones?",
+    ans:"France"},
+    {
+    IDE:5 ,
+    que:"With an area of less than 0.2 square mile (0.5 square kilometer), which is the smallest country in the world?",
+    ans:"Vatican city"},
+    {
+    IDE:6 ,
+    que:"Which continent is home to the most countries?",
+    ans:"Africa"},
+    {
+    IDE:7 ,
+    que:"Home to languages like Tok Pisin, Hiri Motu, and a German creole known as Unserdeutsch, which is the most linguistically diverse country in the world?",
+    ans:"Papua New Guinea"},
+    {
+    IDE:8 ,
+    que:"Which of these is NOT one of the 13 countries crossed by the Equator?",
+    ans:"Egypt"},
+    {
+    IDE:9 ,
+    que:"Thanks to the tombs built by the ancient kingdom of Kush, which country is home to—by far—the most pyramidEs in the world?",
+    ans:"sudan"},
+    {
+    IDE:10 ,
+    que:"What country has the longest coastline?",
+    ans:"Canada"},
 
-
-    {IDE:4, que:"Thanks to its overseas territories, which country technically spans 12 time zones? -france"},
-    {IDE:5, que:"With an area of less than 0.2 square mile (0.5 square kilometer), which is the smallest country in the world? - vatican city"},
-    {IDE:6, que:"Which continent is home to the most countries? -africa"},
     {IDE:7, que:"Home to languages like Tok Pisin, Hiri Motu, and a German creole known as Unserdeutsch, which is the most linguistically diverse country in the world? - Papua New Guinea"},
     {IDE:8, que:"Which of these is NOT one of the 13 countries crossed by the Equator? - egypt"},
     {IDE:9, que:"Thanks to the tombs built by the ancient kingdom of Kush, which country is home to—by far—the most pyramidEs in the world?-sudan"},
