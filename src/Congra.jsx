8e9a688b7f89728c4df97b186f2438df32cecfe8
tsedi lemma <tsedenawit3@gmail.com>
2024-07-10 13:58:38 +0300

    add

--- src/Congra.jsx
+++ src/Congra.jsx
@@ -4,7 +4,11 @@ export default function Congra(){
     return(
         <div id="contain">
             <div id="ima"></div>
-           
+           <h3 className="text-white text-3xl text-center">Congrats! You completed <br></br>the quiz.</h3>
+           <br></br>
+           <br></br>
+           <h3 className="text-white text-xl text-center">You answer 4/10 correctly.</h3>
+           <div id="play"><button>Play again</button></div>
         </div>
     )
 }
