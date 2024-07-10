8e1987714dd4a3fea4d74a91d6485b6c3fc0dd9a
tsedi lemma <tsedenawit3@gmail.com>
2024-07-10 13:47:31 +0300

    s

--- src/Congra.jsx
+++ src/Congra.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Congra(){
     return(
-        <div id="cont">
-            <img src="../public/Images/congrats.png" alt="not found" />
+        <div id="contain">
+            <div id="ima"></div>
+           
         </div>
     )
 }
