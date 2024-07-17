6fb57ab223f45e4994279ee4f25bc242a501abeb
tsedi lemma <tsedenawit3@gmail.com>
2024-07-17 15:59:31 +0300

    sent

--- src/Congra.jsx
+++ src/Congra.jsx
@@ -14,6 +14,7 @@ export default function Congra() {
   const dispatch = useDispatch();
   let play = () => {
     dispatch(setQuestion(Data[Index])) ;
+    navigation("/")
   };
   return (
     <div id="contain">
